5cf2f663db30e7aea8aaf39bb0da22782ed60b48
Guilherme Augusto de Macedo <gaugustomacedo@gmail.com>
2026-03-01 19:32:27 -0300

    add youtube script

---
+++ scripts/.local/bin/youtube-mpv.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+if ! command -v wl-paste &> /dev/null; then
+  echo "Erro: wl-clipboard não está instalado."
+  exit 1
+fi
+
+YOUTUBE_LINK=$(cliphist list | grep -E "^[0-9]+\s+https://www\.youtube\.com/" | head -n 1 | cliphist decode)
+
+if [ -z "$YOUTUBE_LINK" ]; then
+  notify-send "Nenhum link válido na área de transferência." -n avatar-default
+  exit 1
+else
+  /usr/bin/mpv $YOUTUBE_LINK
+fi
