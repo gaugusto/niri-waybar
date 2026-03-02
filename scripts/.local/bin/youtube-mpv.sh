56b8ca5cd23a52c3b3534b4ef29fcc2926db23e6
Guilherme Augusto de Macedo <gaugustomacedo@gmail.com>
2026-03-01 21:35:28 -0300

    app name and low priority on notify send

--- scripts/.local/bin/youtube-mpv.sh
+++ scripts/.local/bin/youtube-mpv.sh
@@ -8,7 +8,7 @@ fi
 YOUTUBE_LINK=$(cliphist list | grep -E "^[0-9]+\s+https://www\.youtube\.com/" | head -n 1 | cliphist decode)
 
 if [ -z "$YOUTUBE_LINK" ]; then
-  notify-send "Nenhum link válido na área de transferência." -n avatar-default
+  notify-send "Nenhum link válido na área de transferência." -n avatar-default -a youtube-mpv.sh -u low
   exit 1
 else
   /usr/bin/mpv $YOUTUBE_LINK
